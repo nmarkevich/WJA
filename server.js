f2c7e06eaf4109233af2a06c222c9c86c9f2b055
nmarkevich <natallia.markevich@gmail.com>
2021-04-26 10:38:03 -0500

    style: Replacing single quotes to double

--- server.js
+++ server.js
@@ -2,23 +2,23 @@
 projectData = {};
 
 // Require Express to run server and routes
-const express = require('express');
+const express = require("express");
 
 // Start up an instance of app
 const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-const cors = require('cors');
+const cors = require("cors");
 app.use(cors());
 
 // Initialize the main project folder
-app.use(express.static('website'));
+app.use(express.static("website"));
 
 
 // Setup Server
