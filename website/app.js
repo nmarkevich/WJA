7b2c94d2e76f77e7e58b3d8118a1c4d68d67b93d
nmarkevich <natallia.markevich@gmail.com>
2021-05-05 16:54:48 -0500

    feat: Adding global variables

--- website/app.js
+++ website/app.js
@@ -1,5 +1,8 @@
 /* Global Variables */
+let baseURL = "api.openweathermap.org/data/2.5/weather?zip=";
+let apiKey = "&appid=24e0a7665603ae3e0a0ffba9db7fe6bd";
+let zipCode;
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth()+"."+ d.getDate()+"."+ d.getFullYear();
