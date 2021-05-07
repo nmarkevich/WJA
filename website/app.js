073fb8711a99c13e01ff6bda10f86016e36fb995
nmarkevich <natallia.markevich@gmail.com>
2021-05-07 17:46:00 -0500

    feat: Adding GET request to the OpenWeatherMap api

--- website/app.js
+++ website/app.js
@@ -6,3 +6,21 @@ let zipCode;
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+"."+ d.getDate()+"."+ d.getFullYear();
+
+document.getElementById("generate").addEventListener("click", generateWeather);
+
+function generateWeather(){
+  const zipCode = document.getElementById("zip").value;
+  getWeather(baseURL, zipCode, apiKey);
+}
+
+const getWeather = async (bURL, zip, key) => {
+  const res = await fetch(bURL+zip+key);
+  try{
+    const data = await res.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.log("error", console.error);
+  }
+}
